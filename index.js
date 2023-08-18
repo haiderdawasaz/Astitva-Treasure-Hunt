0096db965b591748fbf66c07c7fba1b9d04f2965
haiderdawasaz <125685435+haiderdawasaz@users.noreply.github.com>
2023-08-18 13:38:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,76 +1,57 @@
 const Questions = [
     {
         question: "Kedarnath.jpg",
-        answer: "kedarnath"
+        answer: ["kedarnath"]
     },
     {
         question: "Varanasi.jpg",
-        answer: "varanasi"
+        answer: ["varanasi", "banaras"]
     },
     {
         question: "Badrinath.jpg",
-        answer: "badrinath"
+        answer: ["badrinath"]
     },
     {
         question: "Ayodhaya.jpeg",
-        answer: "ayodhaya"
+        answer: ["ayodhaya", "ayodhya"]
     },
     {
         question: "Rameshwaram.jpg",
-        answer: "rameshwaram"
+        answer: ["rameshwaram"]
     },
 ];
 
-let ShuffledQuestions = [];
+let ShuffleQuestions = [];
 let Index = 0;
 let Answer = "";
 
 document.getElementById("Quiz").style.display = "none";
 
-document.getElementById("Intro").addEventListener('keyup', function(event){
+document.getElementById("Intro").addEventListener('keydown', function(event){
     if(event.code === 'Enter'){
         handleIntro();
     }
 });
 
-document.getElementById("Answer").addEventListener('keyup', function(event){
-    if(event.code === 'Enter'){
-        checkAnswer1();
+document.getElementById("Answer").addEventListener('keydown', function(event){
+    if(event.code == 'Enter'){
+        checkAnswer();
     }
 });
 
 function handleQuestions() { 
-    while(ShuffledQuestions.length <= 4){
+    while (ShuffleQuestions.length <= 4) {
         const random = Questions[Math.floor(Math.random() * Questions.length)]
-        if (!ShuffledQuestions.includes(random)) {
-            ShuffledQuestions.push(random)
+        if (!ShuffleQuestions.includes(random)) {
+            ShuffleQuestions.push(random)
         }
     }
 }
 
 handleQuestions();
-function NextQuestion(){
-    const CurrentQuestion = ShuffledQuestions[Index];
-    var pic = document.createElement("img");
-    pic.setAttribute('src', CurrentQuestion.question);
-    document.getElementById("Question").innerHTML = document.body.appendChild(pic);
-    Answer = document.getElementById("Answer").value;
-    Answer = Answer.toLowerCase();
-    checkAnswer(Answer, CurrentQuestion)
-}
-
-function checkAnswer(Answer, CurrentQuestion){
-    if(Answer == CurrentQuestion.answer){
-        ++Index;
-        document.getElementById("Answer").value = "";
-    }
-    else{
-        window.alert("Incorrect Answer");
-    }
-}
 
 function displayQuestion(){
-    const CurrentQuestion = ShuffledQuestions[Index];
+    const CurrentQuestion = ShuffleQuestions[Index];
     if(Index > 4){
         document.getElementById("Quiz").style.display = "none";
         document.getElementById("Outro").style.display = "block";
@@ -79,26 +60,41 @@ function displayQuestion(){
         var pic = document.createElement("img");
         pic.setAttribute('src', CurrentQuestion.question);
         pic.setAttribute('id', "Question");
-        pic.height = 500;
-        pic.width = 750;
+        pic.height = 450;
+        pic.width = 700;
         document.getElementById("Question").replaceWith(pic);
     }
         
 }
 
-function checkAnswer1(){
-    const CurrentQuestion = ShuffledQuestions[Index];
+function checkAnswer(){
+    const CurrentQuestion = ShuffleQuestions[Index];
     Answer = document.getElementById("Answer").value;
     Answer = Answer.toLowerCase();
-    if(Answer == CurrentQuestion.answer){
+    /*if(Answer == CurrentQuestion.answer){
         ++Index;
         document.getElementById("Answer").value = "";
         displayQuestion();
     }
     else{
         window.alert("Incorrect Answer");
+    }*/
+    var check=0;
+    for(var i=0; i <= CurrentQuestion.answer.length; i++){
+        if(CurrentQuestion.answer[i] == Answer){
+            check++;
+            break;
+        }
     }
+    if(check == 1){
+        ++Index;
+        document.getElementById("Answer").value = "";
+        displayQuestion();
+    }
+    else
+        window.alert("Incorrect Answer");
 }
+
 
 function handleIntro(){
     document.getElementById("Info").style.display = "none";
